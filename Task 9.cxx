039ef5f4a43eea8d692237817ff8439a2049b57f
Kashmiri Queen <149133736+Fatima-progmmer@users.noreply.github.com>
2024-03-19 20:55:31 -0700

    Add files via upload

---
+++ Task 9.cxx
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	cout << "Enter a number 1=";
+	cin >> x;
+	cout << "Enter a number 2=";
+	cin >> y;
+	cout << "Addition is =" << x+y << endl;
+	cout << "Substraction is=" << x-y << endl;
+	cout << "Multiplication is=" << x*y<< endl;
+	cout <<"Division is=" <<x/y <<endl;
+	cout << "square of 1 number is=" << x*x <<endl;
+	cout << "square of 2 Number is=" << y*y;
+	return 0;
+}
